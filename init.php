8aefd11eb605f5001f53e7f02059636c99466a3c
Dennis schafroth <dennis@schafroth.com>
2025-05-08 10:01:35 +0200

    Hardcode guardian and DR

--- init.php
+++ init.php
@@ -17,7 +17,7 @@ class remove_media_content extends Plugin {
     }
 
     function hook_feed_fetched($feed_data, $fetch_url, $owner_uid, $feed) {
-	if ($feed != 123) {
+	if ($feed != 100 || $feed != 69) {
 	   return $feed_data;
 	}
         if (!empty($feed_data)) {
