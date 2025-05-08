bc538aafab8d4f6b4f9938842f0e1d2c81872451
Dennis schafroth <dennis@schafroth.com>
2025-05-08 10:41:57 +0200

    Add checkbox for enabling. Only filter enabled

--- init.php
+++ init.php
@@ -16,10 +16,50 @@ class remove_media_content extends Plugin {
     	return 2;
     }
 
-    function hook_feed_fetched($feed_data, $fetch_url, $owner_uid, $feed) {
-	if ($feed != 100 || $feed != 69 || $feed != 106) {
-	   return $feed_data;
+	function hook_prefs_edit_feed($feed_id) {
+		$enabled_feeds = $this->get_stored_array("enabled_feeds");
+		?>
+
+		<header><?= __("Readability") ?></header>
+		<section>
+			<fieldset>
+				<label class='checkbox'>
+					<?= \Controls\checkbox_tag("af_remove_media_content_enabled", in_array($feed_id, $enabled_feeds)) ?>
+					<?= __('Filter Media Content') ?>
+				</label>
+			</fieldset>
+		</section>
+		<?php
 	}
+
+	function hook_prefs_save_feed($feed_id) {
+		$enabled_feeds = $this->get_stored_array("enabled_feeds");
+
+		$enable = checkbox_to_sql_bool($_POST["af_remove_media_content_enabled"] ?? "");
+
+		$enable_key = array_search($feed_id, $enabled_feeds);
+
+		if ($enable) {
+			if ($enable_key === false) {
+				array_push($enabled_feeds, $feed_id);
+			}
+		} else {
+			if ($enable_key !== false) {
+				unset($enabled_feeds[$enable_key]);
+			}
+		}
+
+		$this->host->set($this, "enabled_feeds", $enabled_feeds);
+	}
+
+       function hook_feed_fetched($feed_data, $fetch_url, $owner_uid, $feed) {
+
+		$enabled_feeds = $this->get_stored_array("enabled_feeds");
+		$append_feeds = $this->get_stored_array("append_feeds");
+
+		if (!in_array($feed, $enabled_feeds))
+			return $feed_data;
+
         if (!empty($feed_data)) {
             $doc = new DOMDocument();
             @$doc->loadXML($feed_data);
